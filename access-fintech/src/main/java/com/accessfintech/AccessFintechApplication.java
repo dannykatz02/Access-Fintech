fd16d6f31273d73f914e24c3c2f9b19981fa2e52
Danny Katz <dannykatz02@gmail.com>
2022-09-14 19:23:31 +0300

    initial commit

---
+++ access-fintech/src/main/java/com/accessfintech/AccessFintechApplication.java
@@ -0,0 +1,13 @@
+package com.accessfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessFintechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessFintechApplication.class, args);
+    }
+
+}
